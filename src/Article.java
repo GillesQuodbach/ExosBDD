ef7ce9a44261bd41abf57d9e887d7336b5d7d5dc
Gilles <quodbach.gilles@gmail.com>
2024-01-31 17:41:32 +0100

    exo 3 terminé

---
+++ src/Article.java
@@ -0,0 +1,81 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+
+
+
+
+public class Article {
+	int IdArticle;
+	String Description;
+	String Brand;
+	double UnitaryPrice;
+	int IdCategory;
+	
+
+	
+	public Article(int IdArticle, String Description, String Brand,double UnitaryPrice, int IdCategory) {
+		this.IdArticle = IdArticle;
+		this.Description = Description;
+		this.Brand = Brand;
+		this.UnitaryPrice = UnitaryPrice;
+	}
+	
+	public Article(String Description, String Brand,double UnitaryPrice, int IdCategory) {
+		this.Description = Description;
+		this.Brand = Brand;
+		this.UnitaryPrice = UnitaryPrice;
+		this.IdCategory = IdCategory;
+	}
+
+
+	public int getIdArticle() {
+		return IdArticle;
+	}
+
+	public void setIdArticle(int idArticle) {
+		IdArticle = idArticle;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+	public String getBrand() {
+		return Brand;
+	}
+
+	public void setBrand(String brand) {
+		Brand = brand;
+	}
+
+	public double getUnitaryPrice() {
+		return UnitaryPrice;
+	}
+
+	public void setUnitaryPrice(double unitaryPrice) {
+		UnitaryPrice = unitaryPrice;
+	}
+	
+	public int getIdCategory() {
+		return IdCategory;
+	}
+	
+	public void setIdCategory(int IdCategory) {
+		this.IdCategory = IdCategory;
+	}
+
+	@Override
+	public String toString() {
+		return "Article [IdArticle=" + IdArticle + ", Description=" + Description + ", Brand=" + Brand
+				+ ", UnitaryPrice=" + UnitaryPrice + "]";
+	}
+
+
+
+}
---
+++ src/TestJdbc.java
@@ -0,0 +1,130 @@
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+// EXERCICE 2
+public class TestJdbc {
+	
+	public static ArrayList<Article> articles = new ArrayList<Article>();
+
+
+	//Méthode d'insertion
+	public static void createArticle(Article obj) {
+		String url = "jdbc:mariadb://localhost:3306/shop";
+		String login = "root";
+		String password = "fms2024";
+		String str = "INSERT INTO T_Articles (Description, Brand, UnitaryPrice, IdCategory) VALUES (?,?,?,?);";
+		try (Connection connection = DriverManager.getConnection(url, login, password)) {
+			try (PreparedStatement ps = connection.prepareStatement(str)) {
+				ps.setString(1, obj.getDescription());
+				ps.setString(2, obj.getBrand());
+				ps.setDouble(3, obj.getUnitaryPrice());
+				ps.setInt(4,obj.getIdCategory());
+				if (ps.executeUpdate() == 1) {
+					System.out.println("insertion OK");
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Méthode d'update
+	public static void updateArticle(int articleId, String Description, String Brand, double UnitaryPrice, int IdCategory) {
+		String url = "jdbc:mariadb://localhost:3306/shop";
+		String login = "root";
+		String password = "fms2024";
+		String str = "UPDATE T_Articles SET Description=? ,Brand=?, UnitaryPrice=?, IdCategory=? where IdArticle=?";
+//		for(Article article : articles) {
+//			if (article.getIdArticle() == articleId) {
+//				System.out.println("****************"+article);
+//			}
+//		}
+		try (Connection connection = DriverManager.getConnection(url, login, password)) {
+			try (PreparedStatement ps = connection.prepareStatement(str)) {
+				ps.setString(1, Description);
+				ps.setString(2, Brand);
+				ps.setDouble(3, UnitaryPrice);
+				ps.setInt(4,IdCategory);
+				ps.setInt(5, articleId);
+				if (ps.executeUpdate() == 1) {
+					System.out.println("update OK");
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Méthode de suppression
+		public static void deleteArticle(int articleId) {
+			String url = "jdbc:mariadb://localhost:3306/shop";
+			String login = "root";
+			String password = "fms2024";
+			String str = "DELETE FROM T_Articles where IdArticle=?";
+
+			try (Connection connection = DriverManager.getConnection(url, login, password)) {
+				try (PreparedStatement ps = connection.prepareStatement(str)) {
+					ps.setInt(1, articleId);
+					if (ps.executeUpdate() == 1) {
+						System.out.println("delete OK");
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		
+		
+				//Méthode de visualisation de tout les articles
+				public static void showAllArticles() {
+				String url = "jdbc:mariadb://localhost:3306/shop";
+				String login = "root";
+				String password = "fms2024";
+
+				try (Connection connection = DriverManager.getConnection(url, login, password)) { // connection de java.sql
+					String strSql = "SELECT * FROM T_Articles"; // une fois connecté, réalisation d'une requête
+					try (Statement statement = connection.createStatement()) {
+						try (ResultSet resultSet = statement.executeQuery(strSql)) { // resultSet de java.sql
+							while (resultSet.next()) {
+								int rsIdUser = resultSet.getInt(1); // soit index de 1 a n soit le nom de la colonne
+								String rsDescription = resultSet.getString(2);
+								String rsMarque = resultSet.getString(3);
+								double rsPrixUnitaire = resultSet.getDouble(4);
+								int rsIdCategory = resultSet.getInt(5);
+								articles.add((new Article(rsIdUser, rsDescription, rsMarque, rsPrixUnitaire,rsIdCategory)));
+							}
+						}
+					}
+					for (Article a : articles) {
+						System.out.println(a.getIdArticle() + " - " + a.getDescription() + " - " + a.getBrand() + " - "
+								+ a.getUnitaryPrice()+ " - " + a.getBrand());
+					}
+
+				}catch (Exception e) {
+					e.printStackTrace();
+				}
+				}
+	public static void main(String[] args) throws Exception {
+
+		
+
+//		createPrepared(new Article("Encore Souris", "Apple", 150.0, 1));
+
+		try {
+			// Enregistre la class auprès du driver manager = charge le pilote
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		showAllArticles();
+//		int articleId, String Description, String Brand, double UnitaryPrice, int IdCategory
+//		updatePrepared(23, "SuperNintendo", "NINTENDO", 1000, 3);
+//		deletePrepared(21);
+		createArticle(new Article("Encore Souris", "Apple", 150.0, 1));
+	}
+}
