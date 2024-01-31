e23d5bc8b3f8286b862b8426bdd05227cf9d7d9a
Gilles <quodbach.gilles@gmail.com>
2024-01-31 10:43:08 +0100

    exo 1 terminé

--- Shop.sql
+++ Shop.sql
@@ -30,4 +30,98 @@ INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'Batterie L
 INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'Casque Audio', 'Syno', 105);
 INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'WebCam', 'Logitoch', 755);
 
+
 SELECT * FROM T_Articles;
+
+-- 1.2 ---------------------------
+-- Effectuer les requêtes permettant d’afficher toutes les tables en base
+SHOW TABLES;
+
+-- 1.3 ---------------------------
+-- Trouver un moyen pour décrire une table
+DESCRIBE T_Articles;
+
+-- 1.4 ---------------------------
+-- Ajouter à la table des articles des occurrences de votre choix
+INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'Laptop', 'Lenovo', 1500);
+INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'Ecran', 'ASUS', 155);
+INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'Clavier', 'Logitoch', 100);
+SELECT * FROM T_Articles;
+
+-- 1.5 ---------------------------
+-- Modifier un article avant de vérifier si c’est pris en compte
+UPDATE T_Articles set Brand='Logitech', UnitaryPrice='120' where IdArticle = 15;
+SELECT * FROM T_Articles;
+
+-- 1.6 ---------------------------
+-- Supprimer un article puis vérifier
+DELETE FROM T_Articles where IdArticle = 15;
+SELECT * FROM T_Articles;
+
+-- 1.7 ---------------------------
+-- Sélectionner tous les articles dont le prix est supérieur à 100
+SELECT * FROM T_Articles where UnitaryPrice > 100;
+
+-- 1.8 ---------------------------
+-- Sélectionner les articles dont le prix est compris entre 50 et 150
+SELECT * FROM T_Articles where UnitaryPrice > 50 and UnitaryPrice < 100;
+
+-- 1.9 ---------------------------
+-- Afficher les articles dans l'ordre croissant des prix
+SELECT * from T_Articles ORDER BY UnitaryPrice ASC; 
+
+-- 1.10 ---------------------------
+-- Afficher uniquement la description des articles
+SELECT Description from T_Articles;
+
+-- 1.11 ---------------------------
+-- Choisissez une requête particulièrement intéressante à présenter
+--TODOOOOOOOOOOOOOOOOOOOOOOOO
+
+-- 1.12 ---------------------------
+-- Ajouter la table des catégories à votre base de données et insérez-en
+-- Création nouvelle table
+CREATE TABLE T_Categories (
+	IdCategory INT(4) PRIMARY KEY AUTO_INCREMENT,
+	CatName VARCHAR(30) NOT NULL,
+	Description VARCHAR(100) NOT NULL
+) ENGINE = InnoDB;
+
+-- Ajout de la colonne IdCategory
+ALTER TABLE T_Articles ADD COLUMN IdCategory INT(4);
+ALTER TABLE T_Articles ADD FOREIGN KEY(IdCategory) REFERENCES T_Categories(IdCategory);
+
+-- Insertion catégories
+INSERT INTO T_Categories ( IdCategory, CatName, Description ) VALUES ( 1, 'PC', 'ordinateur portable ou pas');
+INSERT INTO T_Categories ( IdCategory, CatName, Description ) VALUES ( 2, 'Smartphone', 'téléphone de dernière génération avec écran tactile + applis');
+INSERT INTO T_Categories ( IdCategory, CatName, Description ) VALUES ( 3, 'Materiel info', 'Tout matériel informatique en lien avec un ordinateur');
+
+SELECT * from T_Articles;
+
+-- Mise à jour des articles
+UPDATE T_ARTICLES set IdCategory=3 where IdArticle = 1;
+UPDATE T_ARTICLES set IdCategory=3 where IdArticle = 2;
+UPDATE T_ARTICLES set IdCategory=3 where IdArticle = 3;
+UPDATE T_ARTICLES set IdCategory=3 where IdArticle = 4;
+UPDATE T_ARTICLES set IdCategory=3 where IdArticle = 5;
+UPDATE T_ARTICLES set IdCategory=1 where IdArticle = 6;
+UPDATE T_ARTICLES set IdCategory=3 where IdArticle = 7;
+UPDATE T_ARTICLES set IdCategory=3 where IdArticle = 8;
+UPDATE T_ARTICLES set IdCategory=3 where IdArticle = 9;
+UPDATE T_ARTICLES set IdCategory=3 where IdArticle = 10;
+UPDATE T_ARTICLES set IdCategory=3 where IdArticle = 11;
+UPDATE T_ARTICLES set IdCategory=3 where IdArticle = 12;
+UPDATE T_ARTICLES set IdCategory=3 where IdArticle = 13;
+UPDATE T_ARTICLES set IdCategory=3 where IdArticle = 14;
+
+-- Ajout d'un nouvel article
+INSERT INTO T_Articles ( Description, Brand, UnitaryPrice, IdCategory ) VALUES ( 'IPHONE11', 'Apple', 800, 2);
+
+SELECT * FROM T_Articles;
+
+-- 1.13 ---------------------------
+-- Trouver la requête qui permet d’obtenir le résultat suivant :
+SELECT IdArticle, T_Articles.Description, Brand, UnitaryPrice, CatName from T_Articles inner join t_categories where t_articles.idcategory = t_categories.idcategory ORDER BY UnitaryPrice ASC;
+
+
+
